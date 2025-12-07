ac09eb2fe8c872a4348922a41eb41b05233745a9
Dee Lucic <dlucic@gmail.com>
2025-12-07 02:58:00 -0500

    Prepared 2025 files, now using uv to run

---
+++ 2025/Day01/d25p1.py
@@ -0,0 +1,70 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = []
+# ///
+#
+# to add dependencies -> uv add --script <file> <module>
+# to remove dependencies -> uv remove --script <file> <module>
+# to run with test data    -> uv run --script d#p#
+# to run with puzzle data  -> uv run --script d#p# X (any argument)
+
+
+
+# Template Imports
+#----------------------------------------------------------------
+import sys
+import re
+from pathlib import Path
+from pprint import pprint
+from time import time, time_ns
+from collections import deque, Counter, defaultdict
+from functools import partial, lru_cache
+from itertools import product, combinations
+#----------------------------------------------------------------
+
+
+# Solution Code
+#----------------------------------------------------------------
+
+def main(objects: list[str]):
+
+
+
+    print(f"Ans: {ans}")
+
+    # ans: ####
+
+
+
+
+#----------------------------------------------------------------
+
+
+
+# Data load code, and invoking main()
+#----------------------------------------------------------------
+if __name__ == "__main__":
+    # Prep Data
+    lines: list[str]
+
+    if len(sys.argv) == 1:
+        # Paste test example data here
+        test_data = """\
+paste here
+paste here"""
+        lines = test_data.splitlines()
+    else:
+        data_file = Path.cwd() / "puzzle_data.txt"
+        with open(data_file, "r") as f:
+            lines = f.readlines()
+
+    for i, line in enumerate(lines):
+        lines[i] = line.strip()
+    
+    start_time = time()
+    main(lines)
+    print("")
+    print(f"Time in main(): {time() - start_time:.06f}s")
+#----------------------------------------------------------------
+
+
