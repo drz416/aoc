c7fec46e4957e94b63187ef12603973267532657
Dee Lucic <dlucic@gmail.com>
2025-12-13 00:54:36 -0500

    Added Day6 solutions

--- 2025/Day06/d06p1.py
+++ 2025/Day06/d06p1.py
@@ -26,16 +26,33 @@ from itertools import product, combinations
 # Solution Code
 #----------------------------------------------------------------
 
-def main(rows: list[str]):
+def main(lines: list[str]):
 
+    # Delmit each row by consecurtive spaces, built-into str.split()
+    rows: list[list[str]] = []
+    for line in lines:
+        rows.append(line.split())
+    
+    operators: list[str] = rows.pop()
 
-
+    # Go through each problem and perform math
+    num_values = len(rows)
+    
     ans = 0
-
+    for i_prob, operator in enumerate(operators):
+        if operator == "+":
+            prob_ans = 0
+            for i_val in range(num_values):
+                prob_ans += int(rows[i_val][i_prob])
+        else:                                   # opeartor == "*"
+            prob_ans = 1
+            for i_val in range(num_values):
+                prob_ans *= int(rows[i_val][i_prob])
+        ans += prob_ans
     
     print(f"\nAns: {ans}")
 
-    # ans: ####
+    # ans: 4878670269096
 
 
 
@@ -48,24 +65,26 @@ def main(rows: list[str]):
 #----------------------------------------------------------------
 if __name__ == "__main__":
     # Prep Data
-    rows: list[str]
+    lines: list[str]
 
     if len(sys.argv) == 1:
         # Paste test example data here
         test_data = """\
-paste here
-paste here"""
-        rows = test_data.splitlines()
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  """
+        lines = test_data.splitlines()
     else:
         data_file = Path.cwd() / "puzzle_data.txt"
         with open(data_file, "r") as f:
-            rows = f.readlines()
+            lines = f.readlines()
 
-    for i, line in enumerate(rows):
-        rows[i] = line.strip()
+    for i, line in enumerate(lines):
+        lines[i] = line.strip()
     
     start_time = time()
-    main(rows)
+    main(lines)
     print("")
     print(f"Time in main(): {time() - start_time:.06f}s")
 #----------------------------------------------------------------
--- 2025/Day06/d06p2.py
+++ 2025/Day06/d06p2.py
@@ -26,16 +26,37 @@ from itertools import product, combinations
 # Solution Code
 #----------------------------------------------------------------
 
+from math import prod
+
 def main(rows: list[str]):
 
-
+    operators: list[str] = rows.pop()
 
     ans = 0
+    values = []
+    for col in reversed(range(len(operators))):
+        # Scan through each column from right to left (backwards) and
+        # construct values (top to bottom)
+        val = ""
+        for row in rows:
+            digit = row[col]
+            val += "" if digit == " " else digit
+        if val == "":
+            values = []
+            continue
+        values.append(int(val))
 
+        if operators[col] == " ":
+            pass
+        elif operators[col] == "+":
+            ans += sum(values)
+            continue
+        else:
+            ans += prod(values)
     
     print(f"\nAns: {ans}")
 
-    # ans: ####
+    # ans: 8674740488592
 
 
 
@@ -53,16 +74,18 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         # Paste test example data here
         test_data = """\
-paste here
-paste here"""
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  """
         rows = test_data.splitlines()
     else:
         data_file = Path.cwd() / "puzzle_data.txt"
         with open(data_file, "r") as f:
             rows = f.readlines()
 
-    for i, line in enumerate(rows):
-        rows[i] = line.strip()
+    # for i, line in enumerate(rows):
+    #     rows[i] = line.strip()
     
     start_time = time()
     main(rows)
